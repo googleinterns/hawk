7772642e7ef0c78244512fd0b4101e0c7943bf8c
Carmen Popa <carmengpopa@google.com>
2020-07-27 19:41:23 +0000

    added usage instructions
    Reviewed-by: KP Singh <kpsingh@google.com>
    Reviewed-by: Florent Revest <revest@google.com>

--- script/compile_run.sh
+++ script/compile_run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Open a new terminal and use the following command: sudo cat /sys/kernel/debug/tracing/trace"
-
 kcc()
 {
   clang -g -D__TARGET_ARCH_x86 -mlittle-endian -Wno-compare-distinct-pointer-types -O2 -target bpf -emit-llvm -c $1 -o - | llc -march=bpf -mcpu=v2 -filetype=obj -o "$(basename  $1 .c).o";
@@ -9,7 +7,7 @@ kcc()
 
 ucc ()
 {
-    gcc -g $1 -o "$(basename $1 .c)" -I$HOME/libbpf/src/ $HOME/libbpf/src/libbpf.a -lelf -lz
+  gcc -g $1 -o "$(basename $1 .c)" -I$HOME/linux/tools/lib/bpf  $HOME/linux/tools/lib/bpf/libbpf.a -lelf -lz
 }
 
 rm -f ../src/user/user
@@ -17,7 +15,8 @@ rm -f ../src/kern/exec.o
 
 bpftool btf dump file /sys/kernel/btf/vmlinux format c > ../src/kern/vmlinux.h
 kcc ../src/kern/exec.c
-mv  exec.o ../src/kern/exec.o
+mv exec.o ../src/kern/exec.o
+echo "Open a new terminal and use the following command: sudo cat /sys/kernel/debug/tracing/trace"
 
 bpftool gen skeleton ../src/kern/exec.o > ../src/user/exec.skel.h
 ucc ../src/user/user.c
