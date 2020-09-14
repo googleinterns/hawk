a988a792422424d954733278c879e59d5743828e
Carmen Popa <carmengpopa@google.com>
2020-09-14 10:06:18 +0000

    hawk: update README

--- src/main/config.hpp
+++ src/main/config.hpp
@@ -9,13 +9,16 @@ const char DELIMITER = ',';
 class Config
 {
 public:
+	Config();
+	void detect_usecase(int argc, char *argv[]);
+
+	// EXEC MONITOR
 	std::vector<int> ppid;
 	std::vector<std::string> name;
 	int n_proc; /* number of tasks to record */
 
-	Config();
-	void detect_usecase(int argc, char *argv[]);
 	void exec_monitor_parse_args_run();
+
 
 	// VALIDATORS
 	static bool checkSaveAndExportFormat(const char *flagname, const std::string &value);
