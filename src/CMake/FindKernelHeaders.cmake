af349fae909c79a95e2754d42d97ce83d9c67d30
Brendan Jackman <jackmanb@google.com>
2020-10-23 12:45:22 +0000

    Fixup FindKernelHeaders.cmake
    - Fix some errors in the docstrings (e.g. telling you to set
      KernelHeaders_INCLUDE_DIR instead of KernelHeaders_INCLUDE_PATH)
    
    - Ensure that the case where you manually specify header location is distinct
      from the case where we use the system headers. This avoids awkwardness where
      you set a slightly mistaken path and it just silently falls back to building
      against your system headers.

--- src/CMake/FindKernelHeaders.cmake
+++ src/CMake/FindKernelHeaders.cmake
@@ -1,10 +1,17 @@
-FIND_PATH(KernelHeaders_INCLUDE_DIR NAMES "linux/bpf.h" PATHS ${KernelHeaders_INCLUDE_PATH} /include /usr/include DOC "The kernel header directory (path to <linux/bpf.h>)")
+SET(Doc "The kernel header directory (path to <linux/bpf.h>)")
+IF(KernelHeaders_INCLUDE_PATH)
+  FIND_PATH(KernelHeaders_INCLUDE_DIR NAMES "linux/bpf.h" PATHS ${KernelHeaders_INCLUDE_PATH} DOC ${Doc} NO_DEFAULT_PATH)
+ELSE()
+  FIND_PATH(KernelHeaders_INCLUDE_DIR NAMES "linux/bpf.h" DOC ${Doc})
+ENDIF()
+
 
 SET(KernelHeaders_FOUND FALSE)
 
 IF(KernelHeaders_INCLUDE_DIR)
   SET(KernelHeaders_FOUND TRUE)
-  message("Found Linux kernel headers. KernelHeaders_INCLUDE_DIR: ${bpf_INCLUDE_DIR}")
-MESSAGE(WARNING "Couldn't find kernel headers. Have you set KernelHeaders_INCLUDE_PATH to the directory containing linux/bpf.h? Try again with cmake -DKernelHeaders_INCLUDE_DIR=/path/to/kernel/include/uapi/.")
+  message("Found Linux kernel headers. KernelHeaders_INCLUDE_DIR: ${KernelHeaders_INCLUDE_DIR}")
+ELSE()
+  MESSAGE(WARNING "Couldn't find kernel headers. Have you set KernelHeaders_INCLUDE_PATH to the directory containing linux/bpf.h? Try again with cmake -DKernelHeaders_INCLUDE_PATH=/path/to/kernel/usr/include/.")
   SET(KernelHeaders_FOUND FALSE)
 ENDIF()
